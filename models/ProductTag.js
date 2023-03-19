7ada1ef3491203f8a7758c5a742823de5fdf53ef
The-Schwiz <113313870+The-Schwiz@users.noreply.github.com>
2023-03-19 14:35:30 -0400

    complete models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
@@ -7,12 +6,34 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'product',
+          key: 'id',
+          unique: false,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'tag',
+          key: 'id',
+          unique: false,
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
     underscored: true,
+    freezeTableName: true,
     modelName: 'product_tag',
   }
 );
